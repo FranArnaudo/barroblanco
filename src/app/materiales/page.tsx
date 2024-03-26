e29259ee38f032d97be4f2ea08a3e5158d57553c
FranArnaudo <franarnaudo1999@gmail.com>
2024-03-26 01:28:16 -0300

    Small stuff

--- src/app/materiales/page.tsx
+++ src/app/materiales/page.tsx
@@ -20,7 +20,7 @@ const MaterialsPage = async ({
   const { data: materials, totalPages } = await fetchMaterials(searchParams);
   return (
     <div className="w-full flex items-center justify-center ">
-      <div className="w-full flex items-center justify-center flex-col shadow-2xl pt-10 pl-4 pr-5 pb-10 md:m-12 md:p-12  md:mt-0 gap-4 bg-white">
+      <div className="w-full flex items-center justify-center rounded-lg flex-col shadow-2xl pt-10 pl-4 pr-5 pb-10 md:m-12 md:p-12  md:mt-0 gap-4 bg-white">
         <h1 className="text-bold text-4xl self-start">Materiales</h1>
         <MaterialsSearch />
         <div className="w-full">
